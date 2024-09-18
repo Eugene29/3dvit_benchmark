bf19361e1e840f329380114fee5c7407a817baa3
Eugene Ku <eku@uan-0001.head.cm.americas.sgi.com>
2024-09-18 22:02:35 +0000

    add mult_exp for aurora

---
+++ aurora/mult_exp.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+DIR=$(dirname $0)
+export WANDB=1
+
+########################
+## SET UP FOR NODES=1 ##
+########################
+# ##
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=96 PATCH_DIM=4 BS=4 bash $DIR/train_pp.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=96 PATCH_DIM=4 BS=8 bash $DIR/train_pp.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=96 PATCH_DIM=4 BS=16 bash $DIR/train_pp.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=96 PATCH_DIM=4 BS=32 bash $DIR/train_pp.sh
+
+# ##
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=96 PATCH_DIM=16 BS=4 bash $DIR/train_pp.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=96 PATCH_DIM=16 BS=6 bash $DIR/train_pp.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=96 PATCH_DIM=16 BS=8 bash $DIR/train_pp.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=96 PATCH_DIM=16 BS=16 bash $DIR/train_pp.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=96 PATCH_DIM=16 BS=32 bash $DIR/train.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=96 PATCH_DIM=16 BS=60 bash $DIR/train_pp.sh
+H_DIM=768 FFN_SIZE=3072 IMG_DIM=96 PATCH_DIM=16 BS=120 bash $DIR/train_pp.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=96 PATCH_DIM=16 BS=240 bash $DIR/train_pp.sh
+
+# ##
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=256 PATCH_DIM=4 BS=4 bash $DIR/train_pp.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=256 PATCH_DIM=4 BS=8 bash $DIR/train_pp.sh
+
+# ##
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=256 PATCH_DIM=16 BS=4 bash $DIR/train_pp.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=256 PATCH_DIM=16 BS=8 bash $DIR/train_pp.sh
+
+# ##
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=256 PATCH_DIM=64 BS=4 bash $DIR/train_pp.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=256 PATCH_DIM=64 BS=8 bash $DIR/train_pp.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=256 PATCH_DIM=64 BS=16 bash $DIR/train_pp.sh
+# H_DIM=768 FFN_SIZE=3072 IMG_DIM=256 PATCH_DIM=64 BS=32 bash $DIR/train_pp.sh
+
+
+########################
+## SET UP FOR NODES=2 ##
+########################
